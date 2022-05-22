17cfa379f05e09c3127cc66cdbc417878369922e
Defman21 <i@defman.me>
2022-05-22 15:57:30 +0300

    feat(commands): refactor weather command to use geocoder and user location when no argument is given
    Fixed #8

--- lib/maldness_bot/commands/handlers/weather.ex
+++ lib/maldness_bot/commands/handlers/weather.ex
@@ -1,51 +1,52 @@
 defmodule MaldnessBot.Commands.Handlers.Weather do
-  @base_url "https://api.openweathermap.org/data/2.5/weather"
+  @weather_url "https://api.openweathermap.org/data/2.5/weather"
+  @geocode_url "https://api.openweathermap.org/geo/1.0/direct"
+  @config Application.compile_env!(:maldness_bot, MaldnessBot.OpenWeather)
+  @appid @config |> Keyword.fetch!(:key)
+
   alias MaldnessBot.TelegramAPI.API
+  alias MaldnessBot.Models.User
   import MaldnessBot.Gettext
+
+  def handle(arg, message, state) do
+    query_params = %{
+      "units" => Keyword.get(@config, :units, "metric"),
+      "appid" => @appid,
+      "lang" => Map.get(state, :language, "en")
+    }
+
+    case build_query_params(arg, message) do
+      {:ok, search_params} ->
+        Map.merge(query_params, search_params)
+        |> query_openweather()
+        |> send_message(message)
+
+      {:error, "user does not have a location set"} ->
+        send_error_message(message)
+    end
+  end
+
+  defp emoji(2, _), do: "⛈"
+  defp emoji(3, _), do: "🌧"
+  defp emoji(5, _), do: "🌧"
+  defp emoji(6, _), do: "🌨"
+  defp emoji(7, 11), do: "🔥💨"
+  defp emoji(7, 62), do: "🌋💨"
+  defp emoji(7, rem) when rem in [1, 21, 41], do: "🌫"
+  defp emoji(7, rem) when rem in [31, 51, 61], do: "🏜💨"
+  defp emoji(7, rem) when rem in [71, 81], do: "🌪"
+  defp emoji(8, 0), do: "☀️"
+  defp emoji(8, 1), do: "🌤"
+  defp emoji(8, 2), do: "⛅️"
+  defp emoji(8, 3), do: "🌥"
+  defp emoji(8, 4), do: "☁️"
+  defp emoji(_, _), do: ""
 
   defp format_weather(%{"id" => id, "description" => desc}) do
     group = div(id, 100)
     remainder = rem(id, 100)
 
-    icon =
-      case group do
-        2 ->
-          "⛈"
-
-        3 ->
-          "🌧"
-
-        5 ->
-          "🌧"
-
-        6 ->
-          "🌨"
-
-        7 ->
-          case remainder do
-            n when n in [1, 21, 41] -> "🌫"
-            11 -> "🔥💨"
-            n when n in [31, 51, 61] -> "🏜💨"
-            62 -> "🌋💨"
-            n when n in [71, 81] -> "🌪"
-            _ -> ""
-          end
-
-        8 ->
-          case remainder do
-            0 -> "☀️"
-            1 -> "🌤"
-            2 -> "⛅️"
-            3 -> "🌥"
-            4 -> "☁️"
-            _ -> ""
-          end
-
-        _ ->
-          ""
-      end
-
-    [icon, desc] |> Enum.join(" ")
+    [emoji(group, remainder), desc] |> Enum.join(" ")
   end
 
   @spec format_temp(float()) :: binary()
@@ -61,35 +62,57 @@ defmodule MaldnessBot.Commands.Handlers.Weather do
     "#{sign}#{f_temp}"
   end
 
-  def handle(arg, message, state) do
-    open_weather = Application.fetch_env!(:maldness_bot, MaldnessBot.OpenWeather)
+  defp build_query_params(nil, message) do
+    user = User.get_by_telegram(message["from"]["id"])
 
+    with latitude when not is_nil(latitude) <- user.latitude,
+         longitude when not is_nil(longitude) <- user.longitude do
+      {:ok, %{lat: latitude, lon: longitude}}
+    else
+      _ -> {:error, "user does not have a location set"}
+    end
+  end
+
+  defp build_query_params(location, _) do
+    [%{"lat" => latitude, "lon" => longitude}] = query_geocoder(location)
+    {:ok, %{lat: latitude, lon: longitude}}
+  end
+
+  defp query_openweather(params) do
     url =
-      URI.parse(@base_url)
-      |> Map.put(
-        :query,
-        URI.encode_query(%{
-          "units" => Keyword.get(open_weather, :units, "metric"),
-          "appid" => Keyword.fetch!(open_weather, :key),
-          "lang" => Map.get(state, :language, "en"),
-          "q" => arg
-        })
-      )
+      URI.parse(@weather_url)
+      |> Map.put(:query, URI.encode_query(params))
       |> URI.to_string()
 
-    %{
-      "name" => city_name,
-      "main" => %{
-        "temp" => temperature,
-        "feels_like" => feels_like
-      },
-      "weather" => weather
-    } =
-      Finch.build(:get, url)
-      |> Finch.request(MaldnessBot.Finch)
-      |> (fn {:ok, %Finch.Response{body: body}} -> body end).()
-      |> Jason.decode!()
+    Finch.build(:get, url)
+    |> Finch.request(MaldnessBot.Finch)
+    |> (fn {:ok, %Finch.Response{body: body}} -> body end).()
+    |> Jason.decode!()
+  end
 
+  defp query_geocoder(query) do
+    url =
+      URI.parse(@geocode_url)
+      |> Map.put(:query, URI.encode_query(%{q: query, limit: 1, appid: @appid}))
+      |> URI.to_string()
+
+    Finch.build(:get, url)
+    |> Finch.request(MaldnessBot.Finch)
+    |> (fn {:ok, %Finch.Response{body: body}} -> body end).()
+    |> Jason.decode!()
+  end
+
+  defp send_message(
+         %{
+           "name" => city_name,
+           "main" => %{
+             "temp" => temperature,
+             "feels_like" => feels_like
+           },
+           "weather" => weather
+         },
+         message
+       ) do
     description = weather |> Enum.map_join(", ", &format_weather/1)
     temperature = format_temp(temperature)
     feels_like = format_temp(feels_like)
@@ -103,5 +126,17 @@ defmodule MaldnessBot.Commands.Handlers.Weather do
       })
 
     API.send_message(message["chat"]["id"], text)
+    :ok
+  end
+
+  defp send_error_message(message) do
+    API.send_message(
+      message["chat"]["id"],
+      gettext(
+        "Failed to get weather: no argument provided and you don't have a location set. Send a location and reply to it with /set_location."
+      )
+    )
+
+    :ok
   end
 end
